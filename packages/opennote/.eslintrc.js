7eacf309cedcfcec232959bef83f195e071d2c8d
leokyler <42801497+leokyler@users.noreply.github.com>
2026-01-28 14:01:12 +0800

    fix: migrate ESLint config to ESLint 9 flat config format
    - Replace .eslintrc.js with eslint.config.js
    - Remove --ext flag from lint script (not supported in ESLint 9)
    - Remove ESLint 8 dependency, use workspace ESLint 9
    - All tests and lint passing

--- packages/opennote/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["@packages/eslint-config"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  ignorePatterns: ["dist", "node_modules", "coverage"],
-};
